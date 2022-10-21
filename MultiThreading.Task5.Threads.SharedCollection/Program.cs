5d6d4fe53ca79cd991b53d16db77073bfbe41908
Giorgi_Kvirkvelia <Giorgi_Kvirkvelia@EPGETBIW0056>
2022-10-21 21:36:42 +0400

    fix task5

--- MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,6 +5,7 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading;
@@ -15,36 +16,53 @@ namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
-        private static object obj = new object();
+        static List<int> elements = new List<int>();
+        static AutoResetEvent autoReset = new AutoResetEvent(false);
+        static bool producerRunning = true;
 
         static void Main(string[] args)
         {
-            var elements = new List<int>();
+
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
             Console.WriteLine("the first one should add 10 elements into the collection and the second should print all elements in the collection after each adding.");
             Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
             Console.WriteLine();
 
             // feel free to add your code
-            Task.Factory.StartNew(() => AddElements(elements));
+            var addElements = Task.Factory.StartNew(() => AddElements());
+            var printElements = Task.Factory.StartNew(() => PrintElements());
+
+            Task.WaitAll(addElements, printElements);
 
             Console.ReadLine();
         }
 
-        static Task AddElements(List<int> elements)
+        static Task AddElements()
         {
             for (int i = 0; i < 10; i++)
             {
+                autoReset.WaitOne();
                 elements.Add(i);
-                Console.WriteLine();
-
-                var print = Task.Factory.StartNew(() => {
-                    var joinedElements = string.Join(", ", elements);
-                    Console.WriteLine($"[{joinedElements}]");  
-                });
-
-                print.Wait();
+                
             }
+            producerRunning = false;
+            return Task.CompletedTask;
+        }
+
+        static Task PrintElements()
+        {
+            Console.WriteLine();
+
+            while (producerRunning)
+            {
+                autoReset.Set();
+                Console.WriteLine();
+                foreach (var item in elements)
+                {
+                    Console.Write(item.ToString());
+                }
+            }
+
             return Task.CompletedTask;
         }
     }
